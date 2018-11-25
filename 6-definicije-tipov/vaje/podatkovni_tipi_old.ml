c5bcfd68dc95386928d9d5158f64446896596d40
ajdalemut <42772549+ajdalemut@users.noreply.github.com>
2018-11-25 11:15:05 +0100

    resevala vaje in stare mini kolokvije

--- 6-definicije-tipov/vaje/podatkovni_tipi_old.ml
+++ 6-definicije-tipov/vaje/podatkovni_tipi_old.ml
@@ -11,16 +11,15 @@ type race = Orc | Hobbit | Human
    Definiraj tipa, ki predstavljata različne uroke in šole magije.
 *)
 
+type school = Fire | Necrotic | Angelic
 
-type school = ()
+type spell = Blaze | Firewall | Renounce | Banish | Resurrect | Cripple
 
-
-type spell = ()
 
 (* Veščine [skills], ki jih je čarodej osvojil, so seznam vseh urokov,
    ki jih lahko hitro izvede. Definiraj tip `skills'. *)
 
-
+type skills = spell list
 
 (* Čarodeja opišemo z imenom, številom življenskih točk [hp], sposobnost [ability]
    ki jo predstavimo s številom točk mane, raso [race] in veščino [skills].
@@ -29,12 +28,15 @@ type spell = ()
 type mana = int
 type health = int
 
-type wizard = {name : string; }
+type wizard = { name : string; hp : health ; ability : mana ; race : race; skills : skills}
 
 
 (* Napiši funkcijo ki vsakemu uroku priredi primerno šolo magije. *)
+
 let school_of_spell = function
-  () -> ()
+  | Blaze | Firewall -> Fire
+  | Renounce | Banish -> Angelic
+  | Resurrect | Cripple -> Necrotic
 
 (* Glede na tabelo napiši funkcijo, ki uroku priredi količino mane,
    ki jo čarodej potrebuje za izvršitev uroka:
@@ -47,7 +49,13 @@ let school_of_spell = function
 
    Namig: Lahko si pomagaš z regex-replace v Notepad++
  *)
-let mana_of_spell = failwith "todo"
+ let mana_of_spell = function
+  | Blaze -> 420
+  | Firewall -> 35
+  | Renounce -> 17
+  | Banish -> 103
+  | Resurrect -> 178
+  | Cripple -> 250  
 
 (* Ustvari nekaj primerov čarodejov, tako kot je prikazano na primeru Merlina.
    Ponovno si lahko pomagaš s regex-replace.*)
@@ -60,16 +68,26 @@ name : "Snoop Dogg", ability : 420,  hp : 4000,  skills : [Blaze],              
 *)
 
 (* let merlin = {name = "Merlin";   ability = 1832; hp = 9001; skills = [Renounce; Banish];  race = Human} *)
-let frodo =  failwith "todo"
-let ajitam = failwith "todo"
-let mrDuck = failwith "todo"
-let kYloReN = failwith "todo"
-let snoop_dogg = failwith "todo"
+let merlin = {name = "Merlin";   ability = 1832; hp = 9001; skills = [Renounce; Banish];  race = Human}
+let frodo =  {name = "Frodo";    ability = 53;   hp = 1000;  skills = [Renounce];  race = Hobbit}
+let ajitam = {name = "Ajitam";   ability = 1337; hp = 7331; skills = [Firewall; Resurrect; Firewall]; race = Hobbit}
+let mrDuck = {name = "Mr Duck";  ability = 7;    hp = 90000; skills = [Cripple]; race = Orc}
+let kYloReN = {name = "Kylo Ren"; ability = 589;  hp = 90;    skills = [Resurrect]; race = Human}
+let snoop_dogg = {name = "Snoop Dogg"; ability = 420; hp = 4000; skills = [Blaze]; race = Orc}
 
 
 (* Napiši funkcijo, ki iz seznama čarodejev vrne čarodeja z največ mane. *)
 let rec strongest_wizard (wizards : wizard list) : wizard option =
-  failwith "todo"
+  match wizards with
+  | [] -> None
+  | w :: ws ->
+    begin match strongest_wizard ws with
+      | None -> Some w
+      | Some w' ->
+        if w.ability >= w'.ability
+        then Some w
+        else Some w'
+    end
 
 (* Posploši funkcijo strongest_wizard na funkcijo max_list, ki sprejme seznam
    in dodatno funkcijo dveh elementov max : 'a -> 'a -> 'a in vrne maksimalni element seznama
